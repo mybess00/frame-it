ff5c14e644b74d3a2486e49ff82d8119b8e372e5
mybess00 <betancourtrafael00@gmail.com>
2023-07-16 17:03:23 -0400

    update

--- app/pdf/route.js
+++ app/pdf/route.js
@@ -10,6 +10,8 @@ export async function POST (request) {
     })
   }*/
 
+  //
+
   const browser = await puppeteer.connect({
     browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BLESS_TOKEN}`,
   })
